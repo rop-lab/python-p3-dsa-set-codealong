adb187b6f9b8c24b4ad25a236a84e3fd6b965d42
cheruiyotrop <cheruiyot.rop@student.moringaschool.com>
2024-03-16 11:52:53 +0300

    done

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,4 +1,5 @@
 class MySet:
+
     def __init__(self, enumerable = []):
         self.dictionary = {}
         for value in enumerable:
@@ -6,18 +7,23 @@ class MySet:
 
     def has(self, value):
         return value in self.dictionary
-    
+
+    def __str__(self):
+        set_list = []
+        for key, value in self.dictionary.items():
+            set_list.append(str(key))
+        return f'MySet: {{{",".join(set_list)}}}'
+
     def add(self, value):
-        self.dictionary[value] = True
+        self.dictionary[value] = True # Add a value as a key on the Dictionary
         return self
-    
+
     def delete(self, value):
         self.dictionary.pop(value, None)
         return self
-    
+
     def size(self):
         return len(self.dictionary)
-        
-    def __str__(self):
-        """Returns a string representation of the set."""
-        return "{" + ", ".join(map(str, self.elements)) + "}"
+
+    def clear(self):
+        self.dictionary.clear()
